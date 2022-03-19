74f9e02439015037314854389a7c0d97019adb74
iAmFrogger <90090866+iAmFrogger@users.noreply.github.com>
2022-03-19 11:52:22 +0300

    PM4

--- src/JustTal/AllahSupporter/Main.php
+++ src/JustTal/AllahSupporter/Main.php
@@ -4,28 +4,34 @@ declare(strict_types=1);
 
 namespace JustTal\AllahSupporter;
 
+use Heisenburger69\BurgerSpawners\entities\Bat;
+use JsonException;
+use pocketmine\entity\EntityDataHelper;
+use pocketmine\entity\EntityFactory;
+use pocketmine\entity\object\ExperienceOrb;
+use pocketmine\entity\object\ItemEntity;
+use pocketmine\entity\projectile\Projectile;
 use pocketmine\entity\Skin;
-use pocketmine\entity\Entity;
 use pocketmine\event\Listener;
 use pocketmine\event\player\PlayerChatEvent;
 use pocketmine\event\player\PlayerJoinEvent;
-use pocketmine\level\Explosion;
-use pocketmine\level\Position;
+use pocketmine\world\Explosion;
+use pocketmine\nbt\tag\CompoundTag;
 use pocketmine\network\mcpe\protocol\PlaySoundPacket;
 use pocketmine\plugin\PluginBase;
 use pocketmine\resourcepacks\ZippedResourcePack;
 use pocketmine\scheduler\ClosureTask;
+use pocketmine\world\World;
 use ReflectionClass;
-
-use JustTal\AllahSupporter\ArabicLightning;
+use ReflectionException;
 
 class Main extends PluginBase implements Listener {
 
     /** @var Skin $skin */
-    public $skin;
+    public Skin $skin;
 
     /** @var string[] $prayers */
-    public $prayers = [
+    public array $prayers = [
         "allah hu akbar",
         "allah akbar",
         "praise allah",
@@ -33,7 +39,11 @@ class Main extends PluginBase implements Listener {
         "osama bin laden is sexy"
     ];
 
-    public function onEnable() : void {
+	/**
+	 * @throws JsonException
+	 * @throws ReflectionException
+	 */
+	public function onEnable() : void {
         $this->saveResource("skin.png", true);
         $this->saveResource("geometry.json", true);
         $this->saveResource("resource.mcpack", true);
@@ -43,13 +53,17 @@ class Main extends PluginBase implements Listener {
         $this->skin = new Skin("penguin", $this->toBytes(imagecreatefrompng($this->getDataFolder() . "skin.png")), "", "geometry.penguin", file_get_contents($this->getDataFolder() . "geometry.json"));
 
         $this->getServer()->getPluginManager()->registerEvents($this, $this);
-		
-	foreach ([ArabicLightning::class] as $entityClass) { // more to come! >:]
-            Entity::registerEntity($entityClass, true);
-	}
-    }
 
-    public function loadPack() : void {
+		$entityClass = ArabicLightning::class;// more to come! >:]
+		(new EntityFactory)->register($entityClass, function(World $world, CompoundTag $nbt) : Bat{
+			return new Bat(EntityDataHelper::parseLocation($nbt, $world), $nbt);
+		}, ['Lightning', 'minecraft:bat']);
+	}
+
+	/**
+	 * @throws ReflectionException
+	 */
+	public function loadPack() : void {
         $manager = $this->getServer()->getResourcePackManager();
         $pack = new ZippedResourcePack($this->getDataFolder() . "resource.mcpack");
 
@@ -88,29 +102,25 @@ class Main extends PluginBase implements Listener {
 
             $packet = new PlaySoundPacket();
             $packet->soundName = "block.beehive.shear";
-            $packet->x = $player->getX();
-            $packet->y = $player->getY();
-            $packet->z = $player->getZ();
+            $packet->x = $player->getPosition()->getX();
+            $packet->y = $player->getPosition()->getY();
+            $packet->z = $player->getPosition()->getZ();
             $packet->volume = 100;
             $packet->pitch = 1;
 			
-	    $entities = $player->getLevel()->getNearbyEntities($player->getBoundingBox()->expandedCopy(20, 20, 20), $player);
+	    	$entities = $player->getWorld()->getNearbyEntities($player->getBoundingBox()->expandedCopy(20, 20, 20), $player);
             foreach ($entities as $entity) {
                 if ($entity instanceof Projectile) {
-			if ($entity->getOwningEntity() !== $player) {
-                        	$entity->setMotion($entity->getMotion()->multiply(-1));
-                    	}
-                    } else {
-                    	if (!$entity instanceof ItemEntity && !$entity instanceof ExperienceOrb && !isset($entity->namedtag->getValue()["SlapperVersion"])) {
-				$this->getScheduler()->scheduleDelayedTask(new ClosureTask(function (int $currentTick) use ($player, $entity) : void {
-				$x = $entity->getX();
-				$y = $entity->getY();
-				$z = $entity->getZ();
-				$pos = new Position($x, $y, $z);
-				$lightning = Entity::createEntity("ArabicLightning", $player->getLevel(), Entity::createBaseNBT($pos, null, 1, 1));
-				$lightning->setOwningEntity($player);
-				$lightning->spawnToAll();
-			}), 30);
+					if ($entity->getOwningEntity() !== $player) {
+                		$entity->setMotion($entity->getMotion()->multiply(-1));
+            		}
+				} else {
+					if (!$entity instanceof ItemEntity && !$entity instanceof ExperienceOrb && !isset($entity->saveNBT()->getValue()["SlapperVersion"])) {
+						$this->getScheduler()->scheduleDelayedTask(new ClosureTask(function () use ($player, $entity) : void {
+							$lightning = new ArabicLightning($player->getLocation());
+							$lightning->setOwningEntity($player);
+							$lightning->spawnToAll();
+						}), 30);
                     }
                 }
             }
@@ -119,7 +129,7 @@ class Main extends PluginBase implements Listener {
                 $p->sendDataPacket($packet);
             }
 
-            $this->getScheduler()->scheduleDelayedTask(new ClosureTask(function (int $currentTick) use ($player) : void {
+            $this->getScheduler()->scheduleDelayedTask(new ClosureTask(function () use ($player) : void {
                 $explosion = new Explosion($player->getPosition(), 12, $player);
                 $player->setImmobile(false);
                 $player->kill();
@@ -134,7 +144,7 @@ class Main extends PluginBase implements Listener {
         for ($y = 0; $y < imagesy($img); $y++) {
             for ($x = 0; $x < imagesx($img); $x++) {
                 $rgba = @imagecolorat($img, $x, $y);
-                $a = ((~((int)($rgba >> 24))) << 1) & 0xff;
+                $a = ((~($rgba >> 24)) << 1) & 0xff;
                 $r = ($rgba >> 16) & 0xff;
                 $g = ($rgba >> 8) & 0xff;
                 $b = $rgba & 0xff;
